54043e5fec880a1c9699e3c27d44de30145442d4
Miguel <josuecruz1224@outlook.com>
2022-05-16 21:09:15 -0500

    hice cambios en el css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
 import Location from './components/Location';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Location/>
+    <div>
+      <Location /> 
     </div>
   );
 }
--- src/components/ResidentInfo.js
+++ src/components/ResidentInfo.js
@@ -1,41 +1,57 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import "./ResidentInfo.css"
+
 
 const ResidentInfo = ({ url }) => {
     useEffect(() => {
-        axios.get(`${url}`).
-            then(res => setResidents(res.data), setLoading(!loading))
+        axios.get(`${url}`)
+            .then(res => {
+                setResidents(res.data)
+                setLoading(!loading)
+            })
     }, [])
+
     const [residents, setResidents] = useState({})
     const [loading, setLoading] = useState(false)
-    console.log(residents)
+
     return (
         <div>
             {loading === false ?
-                <div>
-                    <img src="https://cdn.dribbble.com/users/233030/screenshots/3932726/rick-sanchez.gif" alt="" />
+                <div className="animation-container">
+                    <div className="animation">
+                        <img src="https://cdn.dribbble.com/users/233030/screenshots/3932726/rick-sanchez.gif" alt="" />
+                    </div>
                 </div>
                 :
-                <div className='ResidentInfo'>
-                <div>
-                    <img src={residents.image} alt="" />
+                <div className="card"
+                    style={{
+                        backgroundImage: `url(${residents.image})`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no-repeat',
+                        width: '100%',
+                        height: '100%',
+                        borderRadius: '10px',
+                        boxShadow: '0px 0px 10px #000000',
+                        margin: '10px',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        color: 'white',
+                        fontSize: '20px',
+                        fontWeight: 'bold',
+                        fontFamily: 'Arial'
+                    }}>
+                    <div className="card_image"><img src={residents.image} alt="" /></div>
+                    <h2 className="card_name">{residents.name}</h2>
+                    <p className="card_title">RAZA<br className="card_text">{residents.species}</br></p>
+                    <p className="card_title">ORIGEN<br className="card_text">{residents.origin?.name}</br></p>
+                    <p className="card_title">Aparición en episodios<br className="card_text">{residents.episode?.length}</br></p>
+                    <button className="btn card_btn">Read More</button>
                 </div>
-                <div>
-                    <p><b>Name: </b>{residents.name} </p>
-                    <p><b>Specie: </b>{residents.species} </p>
-                    <p><b>Status: </b>{residents.status} </p>
-                    <p><b>Origin: </b>{residents.origin?.name} </p>
-                    <p><b>Episodes where appaer: </b>{residents.episode?.length} </p>
-
-                </div>
-
-
-            </div>
-                }
-            
+            }
         </div>
-
     );
 };
 
