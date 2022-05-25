9d98b50467e6b25201e8650ebe9ab8e7eec1192b
rony171998 <rony171998@gmail.com>
2022-05-25 07:01:54 -0500

    barra de busqueda creada

--- src/components/Location.js
+++ src/components/Location.js
@@ -3,12 +3,18 @@ import React, { useEffect, useState } from 'react';
 import "./Location.css";
 import Card from './Card';
 
+import { ReactSearchAutocomplete } from 'react-search-autocomplete'
+
+
+
 
 const Location = () => {
 
     const [resident, setResidents] = useState({})
+    const [dimension, setDimension] = useState({})
     const [id, setId] = useState("")
     const [loading, setLoading] = useState(false)
+
 
     useEffect((loading) => {
         const random = Math.floor(Math.random() * 126) + 1
@@ -17,17 +23,26 @@ const Location = () => {
                 setResidents(res.data)
                 setLoading(!loading);
             })
+        axios.get(`https://rickandmortyapi.com/api/location/`)
+            .then(res => {
+                setDimension(res.data.results)
+            })
     }, [])
 
-
     const searchType = () => {
-        if (id<126 && id>=1) {
-          axios.get(`https://rickandmortyapi.com/api/location/${id}`).then(res => setResidents(res.data))
-        }else{
+        if (id < 126 && id >= 1) {
+            axios.get(`https://rickandmortyapi.com/api/location/${id}`).then(res => setResidents(res.data))
+        } else {
             alert("No existe localizacion")
         }
-        
+
     }
+
+    const handleOnHover = (result) => {
+        setId(result.id)
+    }
+
+    
     return (
         <div >
             {loading === false ?
@@ -43,33 +58,67 @@ const Location = () => {
                         alt="" className="background" />
 
                     <h1 className='title'>Rick and Morty Wiki</h1>
-                    
-                    <input className='input' type="number"
-                        onChange={e => setId(e.target.value)}
-                        value={id}
-                    />
-                    <button onClick={searchType}>Buscar</button>
+
+                    <div className="searchText">
+                        <ReactSearchAutocomplete
+                            items={dimension}
+                            onHover={handleOnHover}
+
+                        />
+                        <button onClick={searchType}>Buscar</button>
+                    </div>
+
+                    <div className='bar-dimensity'>
+
+                        <p><b>Numero: </b></p>
+                        <p><b>Dimension: </b></p>
+                        <p><b>Tipo: </b></p>
+                        <p><b>Nombre: </b></p>
+                        <p><b>Poblacion: </b></p>
+                        <p>{resident.id}</p>
+                        <p>{resident.dimension}</p>
+                        <p>{resident.type}</p>
+                        <p>{resident.name}</p>
+                        <p>{resident.residents.length}</p>
+
+                    </div>
+
                     <div className="main">
 
-                       
                         {
-                        resident.residents?.length === 0 ?  
-                        <div className='people0'>
-                            <h1 className='title'>Sin Poblation</h1>
-                            <img src="https://i.pinimg.com/originals/4d/f4/c5/4df4c5d5342fd4a1c0f6a9ca3bdf2bd8.gif" alt="" />
-                            </div>
-                        :
-                        <ul class="cards-container">
-                            {
-                                resident.residents?.map(resident => (
-                                    <li className="cards_item" key={resident} >
-                                        <Card url={resident} />
-                                    </li>
-                                ))
-                            }
-                        </ul>
+                            resident.residents?.length === 0 ?
+                                <div className='people0'>
+                                    <h1 className='title'>Sin Poblation</h1>
+                                    <img src="https://i.pinimg.com/originals/4d/f4/c5/4df4c5d5342fd4a1c0f6a9ca3bdf2bd8.gif" alt="" />
+                                </div>
+                                :
+                                <ul className="cards-container">
+                                    {
+                                        resident.residents?.map(resident => (
+                                            <li className="cards_item" key={resident} >
+                                                <Card url={resident} />
+                                            </li>
+                                        ))
+                                    }
+                                </ul>
                         }
-                        
+
+                    </div>
+                    <div >
+                        {
+                            resident.residents.length > 9 &&
+                            <div className='pageContent'> 
+                                <h1>1</h1> 
+                                <h1>2</h1> 
+                                <h1>3</h1> 
+                                <h1>4</h1> 
+                                <h1>5</h1> 
+                                <h1>6</h1> 
+
+                                
+                            </div>
+                        }
+
                     </div>
                 </div>
             }
